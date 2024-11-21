7388e47cd0aa04436cddae5d00d105b32fa299e3
DoutorK <143457842+DoutorK@users.noreply.github.com>
2024-11-21 18:24:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import EventScreen from './pages/EventScreen';
 import CreateEvent from './pages/CreateEvent';
+import TodosOsEventos from './pages/TodosOsEventos';
+import SearchResults from './pages/SearchResults';
+import TodosRecentes from './pages/TodosRecentes';
 
 
 export default function App() {
@@ -14,6 +17,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/event/:id" element={<EventScreen />}/>
         <Route path='/Create-Event' element={<CreateEvent/>}/>
+        <Route path='/Found-Event' element={<TodosOsEventos/>}/>
+        <Route path='//search?query=' element={<SearchResults/>}/>
+        <Route path='/Recentes' element={<TodosRecentes/>}/>
       </Routes>
     </BrowserRouter>
     </>
